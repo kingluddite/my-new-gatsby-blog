acb8cb696e2ab8da122d77fd9260dd4a296b657b
Philip Howley <howley.phil@gmail.com>
2023-03-01 19:44:29 +0000

    Complete 3 and 4 of gatsby blog

---
+++ src/templates/BlogPostTemplate.tsx
@@ -0,0 +1,47 @@
+import { MDXProvider } from '@mdx-js/react';
+import { graphql, PageProps } from 'gatsby';
+import { getImage } from 'gatsby-plugin-image';
+import React from 'react';
+import { components, MainContent } from '../components/mdx-components';
+import { PageLayout } from '../components/page-layout';
+
+const BlogPostTemplate: React.FC<PageProps<Queries.BlogPostQuery>> = ({
+  data,
+  children, // ← This is the post content
+}) => {
+  const featuredImage = data.mdx?.frontmatter?.featuredImage
+    ? getImage(data.mdx.frontmatter.featuredImage.childImageSharp)
+    : null;
+
+  return (
+    <PageLayout image={featuredImage} title={data.mdx?.frontmatter?.title ?? undefined}>
+    <MainContent>
+      <div className="mb-8">
+        <span className="text-sm font-thin">
+          By {data.mdx?.frontmatter?.author} on {data.mdx?.frontmatter?.date}
+        </span>
+      </div>
+      <MDXProvider components={components}>{children}</MDXProvider>
+    </MainContent>
+  </PageLayout>
+  );
+};
+
+export default BlogPostTemplate;
+
+export const query = graphql`
+  query BlogPost($id: String!) {
+    mdx(id: { eq: $id }) {
+      frontmatter {
+        title
+        author
+        date(formatString: "MMMM DD, YYYY")
+        featuredImage {
+          childImageSharp {
+            gatsbyImageData(layout: FULL_WIDTH)
+          }
+        }
+      }
+    }
+  }
+`;
